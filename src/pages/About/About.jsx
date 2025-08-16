56c8c4fb9c51d3ad37a244c3e767a4d0f1e5ec03
Yasin <yasinarafat2413@gmail.com>
2025-08-16 10:57:17 +0600

    about section text updated

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,20 +10,25 @@ const About = () => {
                     <MainTitle text={"About Me"}></MainTitle>
                 </div>
 
-                <div>
-                    <p className="text-justify my-4">
-                        I’m a passionate MERN stack developer specializing in
-                        building dynamic, responsive, and user-focused web
-                        applications. With expertise in React, Node.js, Express,
-                        and MongoDB, I craft clean, maintainable code and
-                        seamless user experiences. I enjoy transforming complex
-                        ideas into functional, visually appealing solutions,
-                        always focusing on performance and scalability. Driven
-                        by curiosity and continuous learning, I stay updated
-                        with modern web technologies to deliver innovative
-                        results. My goal is to combine technical skills with
-                        creative problem-solving to build digital products that
-                        truly make an impact.
+                <div className="w-1/2">
+                    <p className="text-justify mt-4 mb-2">
+                        Hello! I’m Md Yasin Arafat, a passionate Frontend & MERN Stack Developer with a strong focus on crafting modern, user-friendly, and scalable web applications. 
+                    </p>
+
+                    <p className="text-justify mb-2">
+                        My programming journey started out of curiosity—experimenting with simple websites, tweaking layouts, and gradually diving deeper into JavaScript then React.
+                    </p>
+
+                    <p className="text-justify mb-2">
+                        I enjoy working on projects where design meets functionality—transforming complex problems into clean, elegant, and intuitive solutions.
+                    </p>
+
+                    <p className="text-justify mb-2">
+                        Outside of coding, I’m a curious explorer at heart. I love playing football, keeping up with tech trends, and sketching ideas when inspiration strikes. These hobbies help me stay balanced, refresh my creativity, and often spark new ways of thinking about my work.
+                    </p>
+
+                    <p className="text-justify mb-2">
+                        At my core, I’m someone who values continuous growth, collaboration, and problem-solving.
                     </p>
                 </div>
             </div>
