48a087935bd2218b18bc52999ca5618566fb5c52
Yasin <yasinarafat2413@gmail.com>
2025-08-16 23:21:53 +0600

    footer section is done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     return (
         <div className="pt-32 pb-4 bg-base-200">
             <div className="flex justify-center items-center">
-                <ul className="flex flex-col gap-4 ">
+                <ul className="flex flex-col gap-4 sm:flex-row">
                     <NavbarLinks></NavbarLinks>
                 </ul>
             </div>
