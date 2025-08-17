90e18abde67acadc55dd6ca3e555918dbe9778d6
Yasin <yasinarafat2413@gmail.com>
2025-08-17 17:12:58 +0600

    go home button added in resume page

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Resume = () => {
     return (
         
-            <div className="h-screen flex justify-center items-center">
+            <div className="h-screen flex flex-col justify-center items-center">
                 <p className="text-center text-2xl text-accent">Resume is not ready yet</p>
+                <p className="my-4">
+                    <Link to="/" className="bg-accent text-white px-2 py-1 hover:bg-accent/80 rounded-md text-xl ">Go Home</Link>
+                </p>
             </div>
         
     );
