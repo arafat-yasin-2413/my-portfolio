0d9cf68ddc3c48e2db89e984fa5bbcba180887d5
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-14 01:09:40 +0600

    resume download feature added in navbar

--- src/components/NavbarLinks/NavbarLinks.jsx
+++ src/components/NavbarLinks/NavbarLinks.jsx
@@ -1,17 +1,56 @@
-import React from 'react';
-import { FaCode, FaEnvelope, FaHome, FaRegFileAlt, FaUser } from 'react-icons/fa';
-import { NavLink } from 'react-router';
+import React from "react";
+import {
+    FaCode,
+    FaEnvelope,
+    FaHome,
+    FaRegFileAlt,
+    FaUser,
+} from "react-icons/fa";
+import { NavLink } from "react-router";
 
 const NavbarLinks = () => {
     return (
-        <
-
-        >
-            <li><NavLink to="/" className="nav  flex items-center gap-1.5 sm:text-[1rem] md:text-[1.1rem] text-white"><FaHome ></FaHome><span>Home</span></NavLink></li>
-            <li><NavLink to="/about" className="nav  flex items-center gap-1 sm:text-[1rem] md:text-[1.1rem] text-white"><FaUser ></FaUser><span>About</span></NavLink></li>
-            <li><NavLink to="/projects" className="nav  flex items-center gap-1 sm:text-[1rem] md:text-[1.1rem] text-white"><FaCode ></FaCode><span>Projects</span></NavLink></li>
-            <li><NavLink to="/contact" className="nav  flex items-center gap-1 sm:text-[1rem] md:text-[1.1rem] text-white"><FaEnvelope ></FaEnvelope><span>Contact</span></NavLink></li>
-            <li><NavLink to="/resume" className="nav  flex items-center gap-1 sm:text-[1rem] md:text-[1.1rem] text-white"><FaRegFileAlt ></FaRegFileAlt><span>Resume</span></NavLink></li>
+        <>
+            <li>
+                <NavLink
+                    to="/"
+                    className="nav  flex items-center gap-1.5 sm:text-[1rem] md:text-[1.1rem] text-white">
+                    <FaHome></FaHome>
+                    <span>Home</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/about"
+                    className="nav  flex items-center gap-1 sm:text-[1rem] md:text-[1.1rem] text-white">
+                    <FaUser></FaUser>
+                    <span>About</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/projects"
+                    className="nav  flex items-center gap-1 sm:text-[1rem] md:text-[1.1rem] text-white">
+                    <FaCode></FaCode>
+                    <span>Projects</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink
+                    to="/contact"
+                    className="nav  flex items-center gap-1 sm:text-[1rem] md:text-[1.1rem] text-white">
+                    <FaEnvelope></FaEnvelope>
+                    <span>Contact</span>
+                </NavLink>
+            </li>
+            <li>
+                <a href="/Resume_of_Md_Yasin_Arafat.pdf" download>
+                    <button className="nav  flex items-center gap-1 sm:text-[1rem] md:text-[1.1rem] text-white cursor-pointer">
+                        <FaRegFileAlt></FaRegFileAlt>
+                        <span>Resume</span>
+                    </button>
+                </a>
+            </li>
         </>
     );
 };
